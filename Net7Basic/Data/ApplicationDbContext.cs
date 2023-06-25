a2c82e262ff9996c91b0243ab515e8b7449a4353
sinancansevinc <sinancan_sevinc@hotmail.com>
2023-06-26 01:52:25 +0300

    Add project files.

---
+++ Net7Basic/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Net7Basic.Models;
+
+namespace Net7Basic.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+    }
+}
